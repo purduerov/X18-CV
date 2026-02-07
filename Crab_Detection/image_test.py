156c73bf9c5026d64a6fada828c532dd58077e28
FakeTonyV2 <fiyinorekoya001@gmail.com>
2026-02-07 14:17:34 -0500

    Code to test a specific image.

---
+++ Crab_Detection/image_test.py
@@ -0,0 +1,20 @@
+from ultralytics import YOLO
+import cv2
+
+model = YOLO(r"C:\Users\User\ROV\X18-CV\Crab_Detection\best.pt")
+
+image_path = r"C:\Users\User\ROV\X18-CV\Crab_Detection\Competition_Image.png"
+
+results = model.predict(
+    source=image_path,
+    conf=0.75,
+    imgsz=640,
+    save=False
+)
+
+annotated_frame = results[0].plot()
+
+cv2.imshow("Crab Detection Fram Test", annotated_frame)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows
